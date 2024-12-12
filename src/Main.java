c17fb4c36d610d816adc33b9b5d9082727f0240e
Ivann MIAVOUDILA <MIAVOUDILA_Iv@lprs.local>
2024-12-12 14:20:04 +0100

    feat(): permettre le choix du nom des joueurs

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,14 @@
+import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Quel est votre nom joueur 1 ?");
+        String joueur1 = scanner.nextLine();
+
+        System.out.println("Quel est votre nom joueur 2 ?");
+        String joueur2 = scanner.nextLine();
+
+        System.out.println("Bonjour " + joueur1 + "et " + joueur2 + ", bon jeu a vous ;)");
     }
 }
